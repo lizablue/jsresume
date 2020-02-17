0c41e1ab789ae06886b7ce4c0f6435d8a97d2d4f
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-02-16 19:17:48 -0600

    additional formating for objectives

--- resume.js
+++ resume.js
@@ -1,14 +1,30 @@
 let coolSkills = ["wakeboarding", "robotics", "python"];
-let mySkills = ["Spanish", "Piano", "Robotics", "DIY Projects", "Wakeboarding", "Robotics", "HTML", "CSS", "Javascript", "Python"]
-let myInterests = ["crafts", "reading", "health", "nature", "science"];
-let pastExperience = ["Accountant - did taxes for individuals and corporationg", "Social Media - managed 30 social media accounts for seceral resorts, a marina, aquarium and nature park", "Digital Marketing - managed digital ads and used Google Analytics to optimize"]
+let mySkills = ["Spanish", "Piano", "Robotics", "DIY Projects", "Wakeboarding", "HTML", "CSS", "Javascript", "Python"]
+
 
 console.log("Name: Liz McCreight");
 console.log("Career: Full Stack Developer");
-console.log("Bio: Licensed Boat Operator");
-
+console.log("Bio: Licensed Boat Driver");
+console.log(" ");
 console.log("My Interests:");
-
+console.log("* crafts");
+console.log("* reading");
+console.log("* health");
+console.log("* nature");
+console.log("* science");
+console.log(" ");
 console.log("My Previous Experience:");
-
+console.log("* Accountant at Martin, Nuñez and Malone, LLC - did taxes for individuals and corporations");
+console.log("* Social Media at Nature Company - managed 30 social media accounts for several resorts, a marina, aquarium and nature park");
+console.log("* Digital Marketing at Resort - managed digital ads and used Google Analytics to optimize");
+console.log(" ");
 console.log("My Skills:");
+console.log("*" + "Spanish");
+console.log("*" + "Piano");
+console.log("*" + "Robotics");
+console.log("*" + "DIY Projects");
+console.log("*" + "Wakeboarding");
+console.log("*" + "HTML");
+console.log("*" + "CSS");
+console.log("*" + "Javascript");
+console.log("*" + "Python");
