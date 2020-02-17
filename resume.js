6704567961c024235fe65b0b3646e6cca0fcbcec
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-02-16 18:55:17 -0600

    objectives

---
+++ resume.js
@@ -0,0 +1,14 @@
+let coolSkills = ["wakeboarding", "robotics", "python"];
+let mySkills = ["Spanish", "Piano", "Robotics", "DIY Projects", "Wakeboarding", "Robotics", "HTML", "CSS", "Javascript", "Python"]
+let myInterests = ["crafts", "reading", "health", "nature", "science"];
+let pastExperience = ["Accountant - did taxes for individuals and corporationg", "Social Media - managed 30 social media accounts for seceral resorts, a marina, aquarium and nature park", "Digital Marketing - managed digital ads and used Google Analytics to optimize"]
+
+console.log("Name: Liz McCreight");
+console.log("Career: Full Stack Developer");
+console.log("Bio: Licensed Boat Operator");
+
+console.log("My Interests:");
+
+console.log("My Previous Experience:");
+
+console.log("My Skills:");
