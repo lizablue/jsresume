369e00d625da9d166b81842833add74885004dcf
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-02-16 19:28:20 -0600

    displayPosition

--- resume.js
+++ resume.js
@@ -3,6 +3,10 @@ let mySkills = ["Spanish", "Piano", "Robotics", "DIY Projects", "Wakeboarding", 
 let name = "liz mccreight";
 let cap = name.toUpperCase();
 
+function displayPosition(company, title, description) {
+    console.log("* " + title + " at " + company + " - " + description);
+};
+
 console.log(cap);
 console.log("Career: Full Stack Developer");
 console.log("Bio: Licensed Boat Driver");
@@ -15,9 +19,9 @@ console.log("* nature");
 console.log("* science");
 console.log(" ");
 console.log("My Previous Experience:");
-console.log("* Accountant at Martin, Nuñez and Malone, LLC - did taxes for individuals and corporations");
-console.log("* Social Media at Nature Company - managed 30 social media accounts for several resorts, a marina, aquarium and nature park");
-console.log("* Digital Marketing at Resort - managed digital ads and used Google Analytics to optimize");
+displayPosition("Martin, Nuñez and Malone, LLC", "Accountant", "did taxes for individuals and corporations");
+displayPosition("Nature Company", "Social Media Manager", "managed social media accounts for several resorts, a marina, aquarium and nature park");
+displayPosition("Resort", "Digital Marketer", "managed digital ads and used Google Analytics to optimize");
 console.log(" ");
 console.log("My Skills:");
 console.log("*" + "Spanish");
